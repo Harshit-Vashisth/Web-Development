1aab3852ae8cf7ea9cf9ff2e20a630bdab8cb8b0
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-09-02 23:22:08 +0530

    h

---
+++ 05 JavaSCRIPT/class 3/index.js
@@ -0,0 +1,24 @@
+// TOPIC 1 math OBJECT  inbuit 
+// MDN 
+Math.random()
+Math.PI
+Math.round(1.2)//1
+Math.round(5.6)//6
+Math.max(211,34,5,6,6);
+
+Math.min(211,34,5,6,6);
+
+
+/// STRINGS (primitive)
+let lastName='Babbar';
+console.log(lastName);
+
+// OBJECT STRING 
+let lastname=new String("babbar");
+console.log(lastname);
+
+
+// convert primitive to obejct  just type dot its the behaviour of js 
+
+lastName.toUpperCase();
+
