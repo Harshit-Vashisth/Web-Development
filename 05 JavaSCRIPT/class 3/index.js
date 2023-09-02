fb4ec9bfee76661f79a82986a2d6acd2b51e7e09
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-09-03 00:15:12 +0530

    Update index.js

--- 05 JavaSCRIPT/class 3/index.js
+++ 05 JavaSCRIPT/class 3/index.js
@@ -38,9 +38,66 @@ let ms='hello \n my name is \' harshit \n i am a full stack dev';
 console.log(ms);
 // \' isse we can use single code to skip esc seq 
 
+let lastame="harshi";
+// now i am going to use 
+// Template literal 
+let message=`this is ${lastame},
+my first msg 
+tata`;
+console.log(message);
+// now it would work fine  `` these are template literal 
+
+
+// object literal {}
+// string literal ''
+// template literal ``
+
+
+// DATE LITERAL
+let date = new Date();
+let date2 = new Date('June 20 1998 07:15');
+let date3 = new Date(1998 ,5, 20, 7); // indexing of month start form 0 
+console.log(date);
+console.log(date2);
+console.log(date3);
+
+
+date3.setFullYear(2002);
+console.log(date3);
 
 
 
+//->> ARRRAYS
+// it is also a type of object 
+// collections of item 
+// bucket whihc can store dif dif type of data 
 
+// creations of array 
+let numbers=[1,2,3,4,6];
+console.log(numbers);
 
+numbers.push(9);
+numbers.unshift(8);
+numbers.splice(2,0,'a',4); // index deletecount  number 
+console.log(numbers);
+console.log(numbers.indexOf(9));
 
+if(numbers.indexOf(7)!=-1)
+    console.log("number is present in the list");
+    console.log(numbers.includes(7));
+    console.log(numbers.includes(9));
+
+    console.log(numbers.indexOf(4,2));
+
+let course=[
+    {no:1,name:'Love'},
+    {no:2 , name:'babbar'}
+];
+// this is an array of object 
+
+console.log(course);
+
+console.log(course.indexOf({no:1,name:'love'}));
+// ans is giving -1 whyyy
+// it is because it is not primitive they both are let diff 
+// in dono ka ref alg alg place pr h 
