57d4a9f81b794fe761329cad6bcbe5a2ebb5095e
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2024-02-01 16:05:15 +0530

    Harshit
    by harshit

---
+++ 05JavaSCRIPT/Class 1/index.js
@@ -0,0 +1,16 @@
+console.log('Heloo');
+let marks=90;
+if(marks>90)
+    console.log("a");
+else if (marks>80)
+console.log('b');
+else
+console.log('fail');
+
+switch(marks){
+    case 90: console.log('45'); 
+    break;
+    case 92: console.log("hemlo");
+    break;
+    default: console.log("ads");
+}
---
+++ 05JavaSCRIPT/Class 1/index1.js
@@ -0,0 +1,39 @@
+// console.log("hearshi");
+// const rect={
+//         length:5,
+//         breath:3,
+//         draw: function(){
+//             console.log('draw');
+//         }
+// }; 
+
+//thsi is not excatly fucntion ,, if we are creating a fucntion under object it is kno-wing as method 
+
+
+//to create multiple rectangle in the code as object ......
+//function to create object 
+function createRect(len,bre){
+    return rect={ 
+        l:len,
+        b:bre,
+        draw: function(){
+            console.log("draw");
+        }
+    };
+}
+let rectobj1=createRect(3,5);
+
+//constructor fucniton -> prop or methods or initialsie karta h ye we dont need to return here 
+//pascal notation is there .. frist letter of every word is capital 
+function Rect(){
+    // right now reffer to empty 
+    this.length=2;
+    this.breadth=3;
+    this.draw=function(){
+        console.log("hello"); 
+    }
+    //this is the current object 
+}
+
+//object creation using rectangle 
+let rectangleobj=new Rect();
