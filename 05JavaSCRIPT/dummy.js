78e1366620c243ff04027dd087ef3b6182560d09
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2024-03-30 17:57:38 +0530

    Update dummy.js

--- 05JavaSCRIPT/dummy.js
+++ 05JavaSCRIPT/dummy.js
@@ -20,10 +20,47 @@ console.log(false||2||3);
 //undefined, null . 0 . false. '' . NAN    these all are falsey
 let exp=2;
 switch(exp){
-    case 1: console.log('hi');
+    case 1: console.log('hi'); 
     case 2: console.log('hello');
 }
 
 for(let i=0;i<5;i++){
     console.log(i);
 }
+const remt={
+    length:1,
+    breadth:2,
+    draw: function(){
+        console.log("ho");
+    }
+};
+
+function createrect(l,b){
+    return rect= {
+        length:l,
+        breath:b,
+        draw(){
+            console.log("shdi");
+        }
+    };
+};
+
+let reato1=createrect(2,3); 
+
+function createe(l,b){
+this.length=l;
+this.breath=b;
+this.draw=function(){
+    console.log('draw');
+}
+}
+let rectobj=new createe(4,5);
+rectobj.color='yello';
+
+let r1=new Function('l','b',`this.length=l;
+this.breath=b;
+this.draw=function(){
+    console.log('draw');
+}`);
+let ro1=new r1(2,5);
+console.log(rect);
